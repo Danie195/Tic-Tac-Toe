326882230d1eb47782db96e11c29b4403e9460b9
Danie195 <danie195@purdue.edu>
2021-11-10 11:16:22 -0500

    Pre-Alpha 0.1

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,1 +1,42 @@
 # Tic-Tac-Toe using Pygame. It's practice for me to be more focused on using object-oriented programming
+
+# NEED TO DO:
+# Create cursor and click system
+# Create grid
+# Create assets
+# Create game logic
+# Create win/lose screen
+# Create AI?
+
+import pygame
+import sys
+
+
+class main:
+    pygame.init()
+
+    # Screen and clocl
+    displayWidth = 400
+    displayHeight = 400
+    screen = pygame.display.set_mode((displayWidth, displayHeight))
+    clock = pygame.time.Clock()
+    FPS = 60
+
+    # Colors
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+                sys.exit()
+
+        screen.fill(white)
+        pygame.display.update()
+        clock.tick(FPS)
+
+
+main()
